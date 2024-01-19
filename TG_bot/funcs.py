a6d0147f803f364b91d060b1b4feecc245402ab7
razy eloyznik <kee.ukue@yandex.ru>
2024-01-19 04:47:37 +0000

    19.01.24

--- TG_bot/funcs.py
+++ TG_bot/funcs.py
@@ -9,8 +9,8 @@ def create_titles_and_markup_by_data(data:list):
         markup.add(KeyboardButton(title))
     return (titles, markup)	
 def get_subjects():
-    data = requests.get("/ru/api/subjects/").json()
+    data = requests.get("ru/api/subjects/").json()
     return create_titles_and_markup_by_data(data)
 def get_courses():
-    data = requests.get("/ru/api/courses/").json()
+    data = requests.get("ru/api/courses/").json()
     return create_titles_and_markup_by_data(data)
