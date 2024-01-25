4d447c429cbd573c80ff6490ecb86eec1af3dc23
Ael914 <dla3lon@gmail.com>
2024-01-25 16:00:33 +0700

    Теперь работает чатъ

--- bongalearns/bongalearns/asgi.py
+++ bongalearns/bongalearns/asgi.py
@@ -9,18 +9,18 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
+import chat.routing
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-
-import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bongalearns.settings")
 django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(
+        "websocket": AllowedHostsOriginValidator(
             URLRouter(chat.routing.websocket_urlpatterns),
         ),
     }
